06eefe920a768e86c3fb384049d1495fa262e7c1
GreenVVich <96542151+GreenVVich@users.noreply.github.com>
2021-12-23 12:08:25 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -44,7 +44,7 @@ int main(int argc, char** argv) {
     h = A[0][1] + vy*t - 9.81*t*t/2;
 
     //if (h > A[n][1]){}
-    if (h<=A[n][1]){
+    if (h < A[n][1]){
         total = A[n][2]-1;
         if (k == -1){
             total = total+1;
